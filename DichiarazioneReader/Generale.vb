1c741ed98146e395d7b1950f4b5d4d1b0a774df8
matteodistefano <info@matteodistefano.it>
2025-10-01 18:45:55 +0200

    v2

--- DichiarazioneReader/Generale.vb
+++ DichiarazioneReader/Generale.vb
@@ -3,6 +3,7 @@
         Dim Par() As String = My.Application.CommandLineArgs.ToArray
         Dim DA As DAL.DatabaseSpecific.DataAccessAdapter = Nothing
 
+
         Dim cfgPath As String = mdsUtility.FileSystem.GetUserConfigFolder(True) & "\settings.xml"
         cfgPath = cfgPath.Replace(My.Application.Info.AssemblyName, "PoliecoSoci")
         If Not System.IO.File.Exists(cfgPath) Then
